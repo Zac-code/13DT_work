7bea50910647e97ffb92e118caacb7a2c9067080
Zac-code <zlindsay@students.mags.school.nz>
2026-06-15 14:46:44 +1200

    doing some more work

--- 13-dtdungeonfighter/scripts(pain)/playerscript.gd
+++ 13-dtdungeonfighter/scripts(pain)/playerscript.gd
@@ -5,6 +5,10 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
+var score : int = 0
+var double_jump : bool = true
+@export var ui : Node
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -13,6 +17,16 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	if is_on_floor() and not double_jump:
+		double_jump = true
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor() or (not is_on_floor() and double_jump):
+			if not is_on_floor():
+				double_jump = false
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
