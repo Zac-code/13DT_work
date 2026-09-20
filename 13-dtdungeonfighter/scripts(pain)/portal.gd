ab718ba456e7557e17fee517694dbd2caecdf8fb
Zac-code <zlindsay@students.mags.school.nz>
2026-09-21 09:38:35 +1200

    working on portal

--- 13-dtdungeonfighter/scripts(pain)/portal.gd
+++ 13-dtdungeonfighter/scripts(pain)/portal.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
-@export_file("*.tscn") var destination_scene: String = "res://level2.tscn"
 
-func _on_portal_entered(body: Node2D) -> void:
-	if body.name == "player":
-		get_tree().change_scene_to_file(destination_scene)
+func _on_body_entered(body: Node2D) -> void:
+	if body.is.in_group("Player"):
+		pass
