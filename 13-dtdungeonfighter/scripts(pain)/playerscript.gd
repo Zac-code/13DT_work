5191061488d90187311b6c978056270742ffa5e8
Zac-code <zlindsay@students.mags.school.nz>
2026-09-24 10:04:39 +1200

    jumping counter

--- 13-dtdungeonfighter/scripts(pain)/playerscript.gd
+++ 13-dtdungeonfighter/scripts(pain)/playerscript.gd
@@ -4,11 +4,12 @@ const SPEED := 300.0
 const JUMP_VELOCITY := -400.0
 const MAX_HEALTH := 3
 
+
 var score: int = 0
 var double_jump: bool = true
 var health := 3
 
-@export var ui: Node
+@export var ui: Label
 
 
 func _physics_process(delta: float) -> void:
@@ -20,8 +21,14 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_on_floor() or double_jump:
+			score += 1
+
+			if ui is Label:
+				ui.text = "Jumps: " + str(score)
+
 			if not is_on_floor():
 				double_jump = false
+
 			velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -36,6 +43,6 @@ func _physics_process(delta: float) -> void:
 
 func hit() -> void:
 	health -= 1
-	
+
 	if health <= 0:
 		get_tree().call_deferred("reload_current_scene")
