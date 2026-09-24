ee7e0029bb556c44920caef37b95531c16f87270
Zac-code <zlindsay@students.mags.school.nz>
2026-09-24 14:59:05 +1200

    urgefns

--- 13-dtdungeonfighter/scripts(pain)/level_1.gd
+++ 13-dtdungeonfighter/scripts(pain)/level_1.gd
@@ -1,18 +1,17 @@
 extends Node2D
-@onready var pause_menu = "res://assets/pause.tscn"
+
+@onready var pause_menu = $PauseMenu
 var paused = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if input.is_action_just_pressed("pause"):
-		PauseMenu()
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		toggle_pause()
 
-func pausemenu():
-	if paused
+func toggle_pause() -> void:
+	if paused:
 		pause_menu.hide()
 		Engine.time_scale = 1
 	else:
 		pause_menu.show()
 		Engine.time_scale = 0
-
 	paused = !paused
